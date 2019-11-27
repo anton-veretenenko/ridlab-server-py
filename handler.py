3925585a2feeac6afa20b768a3cb5eaad3bd473f
anton-veretenenko <anton@veretenenko.ru>
2019-11-28 03:36:29 +0500

    request parser and file handler placeholder

--- handler.py
+++ handler.py
@@ -2,6 +2,29 @@ import select
 import os
 import socket
 
+class FHandler:
+
+    __file = None
+    __size = 0
+    __pos = 0
+
+    def __init__(self, request, socket):
+        self.__socket = socket
+        data = request.strip().split()
+        print(data)
+
+    @property
+    def file(self):
+        return self.__file
+    
+    @property
+    def size(self):
+        return self.__size
+    
+    def send(self):
+        return False # in case of failed send
+        pass
+
 class SEHandler:
 
     __resp_200 = b'HTTP/1.0 200 OK\r\n\r\n'
@@ -13,6 +36,7 @@ class SEHandler:
         self.__epoll = epoll
         self.__datain = b''
         self.__dataout = b''
+        self.__fhandler = None
     
     def pollin(self):
         datain = self.__socket.recv(1024)
@@ -25,21 +49,33 @@ class SEHandler:
             if b'\n\n' in self.__datain or b'\r\n\r\n' in self.__datain:
                 # end of request detected, process and swith to output
                 self.__epoll.modify(self.__socket.fileno(), select.EPOLLOUT)
-                self.__dataout = self.__resp_cont % 2
+                self.__fhandler = FHandler(self.__datain, socket)
+                if self.__fhandler.file is None:
+                    self.__dataout = self.__resp_404;
+                else:
+                    self.__dataout = self.__resp_200 + (self.__resp_cont % self.__fhandler.size)
 
     def pollout(self):
-        # put out dataout
-        written = self.__socket.send(self.__dataout)
-        self.__dataout = self.__dataout[written:]
-        # and check if there are any left
-        if len(self.__dataout) == 0:
-            self.__epoll.modify(self.__socket.fileno(), 0)
-            self._close_force();
+        if len(self.__dataout) > 0:
+            # put out header
+            written = self.__socket.send(self.__dataout)
+            self.__dataout = self.__dataout[written:]
+        elif self.__fhandler.file is not None:
+            # send file if found
+            if self.__fhandler.send() == False:
+                # end transmission if file ended
+                self._end()
+        else:
+            self._end()
 
     def pollhup(self):
         self.__epoll.unregister(self.__socket.fileno())
         self._close()
         self._del()
+    
+    def _end(self):
+        self.__epoll.modify(self.__socket.fileno(), 0)
+        self._close_force()
 
     def _close_force(self):
         self.__socket.shutdown(socket.SHUT_RDWR)
@@ -47,5 +83,5 @@ class SEHandler:
     def _close(self):
         self.__socket.close()
     
-    def _del(self) :
+    def _del(self):
         del self.__socket
